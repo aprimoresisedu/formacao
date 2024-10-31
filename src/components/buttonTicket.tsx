3c99472198fe87f2f67c5abccbf8ffb3bc090e7b
aprimoresisedu <contato@aprimoresisedu.com>
2024-10-31 14:48:37 -0300

    Update buttonTicket.tsx

--- src/components/buttonTicket.tsx
+++ src/components/buttonTicket.tsx
@@ -20,7 +20,7 @@ export default function ButtonTicket({ href, label, className, classNameB, searc
             <div className={`w-full flex ${className ? className : 'max-w-sm'}`}>
                 <a
                     className={`btn-grad px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
-                    href={`https://chk.eduzz.com/39VJP8N59R/?utm_term=${searchParams.utm_term}&utm_campaign=${searchParams.utm_campaign}&utm_source=FacebookAds-p4&utm_content=${searchParams.utm_content}&utm_medium=${searchParams.utm_medium}`}
+                    href={`https://chk.eduzz.com/39VJP8N59R?utm_term=${searchParams.utm_term}&utm_campaign=${searchParams.utm_campaign}&utm_source=FacebookAds-p4&utm_content=${searchParams.utm_content}&utm_medium=${searchParams.utm_medium}`}
 
                 >{label}</a>
             </div>
@@ -31,7 +31,7 @@ export default function ButtonTicket({ href, label, className, classNameB, searc
         <div className={`w-full flex ${className ? className : 'max-w-sm'}`}>
             <a
                 className={`btn-grad px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
-                href={`https://chk.eduzz.com/39VJP8N59R/?utm_term=${'nao-treaqueado'}&utm_campaign=${'nao-treaqueado'}&utm_source=FacebookAds-p3&utm_content=${'nao-treaqueado'}&utm_medium=${'nao-treaqueado'}`}
+                href={`https://chk.eduzz.com/39VJP8N59R?utm_term=${'nao-treaqueado'}&utm_campaign=${'nao-treaqueado'}&utm_source=FacebookAds-p3&utm_content=${'nao-treaqueado'}&utm_medium=${'nao-treaqueado'}`}
             >{label}</a>
         </div>
     )
