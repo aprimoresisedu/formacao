574fe2a03ac8a4a8a19fa2a057f5970eb6145a47
leafartech <119262053+leafartech@users.noreply.github.com>
2024-11-08 10:06:58 -0300

    opa

--- src/components/buttonT2.tsx
+++ src/components/buttonT2.tsx
@@ -20,7 +20,7 @@ export default function ButtonT2({ href, label, className, classNameB, searchPar
             <div className={`w-full flex ${className ? className : 'max-w-sm'}`}>
                 <a
                     className={` border-8 border-[#608c98]/60  bg-[#00c9ff] px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
-                    href={`https://chk.eduzz.com/2460920?utm_term=${searchParams.utm_term}&utm_campaign=${searchParams.utm_campaign}&utm_source=FacebookAds-p4&utm_content=${searchParams.utm_content}&utm_medium=${searchParams.utm_medium}`}
+                    href={`https://chk.eduzz.com/2460920?utm_term=${searchParams.utm_term}&utm_campaign=${searchParams.utm_campaign}&utm_source=${searchParams.utm_source}&utm_content=${searchParams.utm_content}&utm_medium=${searchParams.utm_medium}`}
 
                 >{label}</a>
             </div>
@@ -31,7 +31,7 @@ export default function ButtonT2({ href, label, className, classNameB, searchPar
         <div className={`w-full flex ${className ? className : 'max-w-sm'}`}>
             <a
                 className={` border-8 border-[#608c98]/60  bg-[#00c9ff] px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
-                href={`https://chk.eduzz.com/2460920?utm_term=${'nao-treaqueado'}&utm_campaign=${'nao-treaqueado'}&utm_source=FacebookAds-p3&utm_content=${'nao-treaqueado'}&utm_medium=${'nao-treaqueado'}`}
+                href={`https://chk.eduzz.com/2460920?utm_term=${'nao-treaqueado'}&utm_campaign=${'nao-treaqueado'}&utm_source=${'nao-traqueado'}&utm_content=${'nao-treaqueado'}&utm_medium=${'nao-treaqueado'}`}
             >{label}</a>
         </div>
     )
