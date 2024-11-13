4cc375a0a1b8c9bb5de064c4f97b2844e60cb3c2
leafartech <119262053+leafartech@users.noreply.github.com>
2024-11-12 22:50:43 -0300

    opa

--- src/components/buttonTicket.tsx
+++ src/components/buttonTicket.tsx
@@ -22,7 +22,7 @@ export default function ButtonTicket({ href, label, className, classNameB, searc
                 <a
                     // className={`btn-grad px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
                     className={`bg-gradient-to-b from-red-500 to-red-600 border-4 border-red-700 px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
-                    href={`${btn_url}?utm_term=${searchParams.utm_term}&utm_campaign=${searchParams.utm_campaign}&utm_source=FacebookAds-p4&utm_content=${searchParams.utm_content}&utm_medium=${searchParams.utm_medium}`}
+                    href={`${btn_url}?utm_term=${searchParams.utm_term}&utm_campaign=${searchParams.utm_campaign}&utm_source=${searchParams.utm_source}&utm_content=${searchParams.utm_content}&utm_medium=${searchParams.utm_medium}`}
 
                 >{label}</a>
             </div>
@@ -34,7 +34,7 @@ export default function ButtonTicket({ href, label, className, classNameB, searc
             <a
                 // className={`btn-grad px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
                 className={`bg-gradient-to-b from-red-500 to-red-600 border-4 border-red-700 px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
-                href={`${btn_url}?utm_term=${'nao-treaqueado'}&utm_campaign=${'nao-treaqueado'}&utm_source=FacebookAds-p3&utm_content=${'nao-treaqueado'}&utm_medium=${'nao-treaqueado'}`}
+                href={`${btn_url}?utm_term=${'nao-treaqueado'}&utm_campaign=${'nao-treaqueado'}&utm_source=nao-traqueado&utm_content=${'nao-treaqueado'}&utm_medium=${'nao-treaqueado'}`}
             >{label}</a>
         </div>
     )
