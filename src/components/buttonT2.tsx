e27f8612db52ea49f0de30178d020c15caf623a4
leafartech <119262053+leafartech@users.noreply.github.com>
2024-11-12 22:45:50 -0300

    opa

--- src/components/buttonT2.tsx
+++ src/components/buttonT2.tsx
@@ -20,7 +20,7 @@ export default function ButtonT2({ href, label, className, classNameB, searchPar
             <div className={`w-full flex ${className ? className : 'max-w-sm'}`}>
                 <a
                     // className={` border-8 border-[#608c98]/60  bg-[#00c9ff] px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
-                    className={` border-8 border-[#608c98]/60  bg-red-500 px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
+                    className={` border-8 border-[#608c98]/60  bg-gradient-to-b from-red-500 to-red-600 px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
                     href={`https://chk.eduzz.com/2460920?utm_term=${searchParams.utm_term}&utm_campaign=${searchParams.utm_campaign}&utm_source=${searchParams.utm_source}&utm_content=${searchParams.utm_content}&utm_medium=${searchParams.utm_medium}`}
 
                 >{label}</a>
@@ -32,7 +32,7 @@ export default function ButtonT2({ href, label, className, classNameB, searchPar
         <div className={`w-full flex ${className ? className : 'max-w-sm'}`}>
             <a
                 // className={` border-8 border-[#608c98]/60  bg-[#00c9ff] px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
-                className={` border-8 border-[#608c98]/60  bg-red-500 px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
+                className={` border-8 border-[#608c98]/60  bg-gradient-to-b from-red-500 to-red-600 px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
                 href={`https://chk.eduzz.com/2460920?utm_term=${'nao-treaqueado'}&utm_campaign=${'nao-treaqueado'}&utm_source=${'nao-traqueado'}&utm_content=${'nao-treaqueado'}&utm_medium=${'nao-treaqueado'}`}
             >{label}</a>
         </div>
--- src/components/buttonTicket.tsx
+++ src/components/buttonTicket.tsx
@@ -11,17 +11,18 @@ interface ButtonProps {
         utm_source?: string
         utm_medium?: string
     } 
+    btn_url?: string
 }
 
-export default function ButtonTicket({ href, label, className, classNameB, searchParams }: ButtonProps) { 
+export default function ButtonTicket({ href, label, className, classNameB, searchParams, btn_url }: ButtonProps) { 
 
     if (searchParams.utm_campaign) {
         return (
             <div className={`w-full flex ${className ? className : 'max-w-sm'}`}>
                 <a
                     // className={`btn-grad px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
-                    className={`bg-red-500 px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
-                    href={`https://chk.eduzz.com/39VJP8N59R?utm_term=${searchParams.utm_term}&utm_campaign=${searchParams.utm_campaign}&utm_source=FacebookAds-p4&utm_content=${searchParams.utm_content}&utm_medium=${searchParams.utm_medium}`}
+                    className={`bg-gradient-to-b from-red-500 to-red-600 border-4 border-red-700 px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
+                    href={`${btn_url}?utm_term=${searchParams.utm_term}&utm_campaign=${searchParams.utm_campaign}&utm_source=FacebookAds-p4&utm_content=${searchParams.utm_content}&utm_medium=${searchParams.utm_medium}`}
 
                 >{label}</a>
             </div>
@@ -32,8 +33,8 @@ export default function ButtonTicket({ href, label, className, classNameB, searc
         <div className={`w-full flex ${className ? className : 'max-w-sm'}`}>
             <a
                 // className={`btn-grad px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
-                className={`bg-red-500 px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
-                href={`https://chk.eduzz.com/39VJP8N59R?utm_term=${'nao-treaqueado'}&utm_campaign=${'nao-treaqueado'}&utm_source=FacebookAds-p3&utm_content=${'nao-treaqueado'}&utm_medium=${'nao-treaqueado'}`}
+                className={`bg-gradient-to-b from-red-500 to-red-600 border-4 border-red-700 px-4 py-2 text-xl font-bold uppercase rounded-full ${classNameB}`}
+                href={`${btn_url}?utm_term=${'nao-treaqueado'}&utm_campaign=${'nao-treaqueado'}&utm_source=FacebookAds-p3&utm_content=${'nao-treaqueado'}&utm_medium=${'nao-treaqueado'}`}
             >{label}</a>
         </div>
     )
