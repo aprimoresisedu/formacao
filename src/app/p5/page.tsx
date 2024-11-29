827a2470c9b156972a6c6c38e0913e0f1c3b3668
aprimoresisedu <contato@aprimoresisedu.com>
2024-11-29 09:54:33 -0300

    Update page.tsx

--- src/app/p5/page.tsx
+++ src/app/p5/page.tsx
@@ -211,13 +211,7 @@ export default function Page({ searchParams }: FormProps) {
                                         <svg className="h-6 w-6 absolute top-[1px] left-0 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                         </svg>
-                                        <span><strong></strong> para antecipação e organização de equipe</span>
-                                    </li>
-                                    <li className="relative ps-8 text-white">
-                                        <svg className="h-6 w-6 absolute top-[1px] left-0 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                        </svg>
-                                        <span><strong>Checklist</strong> de funções, ações dos profissionais e recursos do setor</span>
+                                        <span><strong>Checklist</strong> de funções, ações dos profissionais e recursos do setor para antecipação e organização de equipe</span>
                                     </li>
                                     <li className="relative ps-8 text-white">
                                         <svg className="h-6 w-6 absolute top-[1px] left-0 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor">
