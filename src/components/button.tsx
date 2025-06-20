71dd7de8022c2992fe04703cb9eef9e9d8d5813c
Rafael Borges Bezerra <leafartech@gmail.com>
2025-06-20 12:22:34 -0300

    updt

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -15,29 +15,30 @@ interface ButtonProps {
 
 export default function Button({ href, label, className, classNameB, searchParams }: ButtonProps) {
 
-    if (href) {
-        return (
-            <div className={`w-full flex ${className ? className : 'max-w-sm'}`}>
-                <a
-                    className={`px-16 sm:px-20 btn-grad-2 px-4 py-3 text-xl font-bold uppercase rounded-full ${classNameB}`}
-                    href={`${href}?utm_term=${searchParams.utm_term}&utm_campaign=${searchParams.utm_campaign}&utm_source=${searchParams.utm_source}&utm_content=${searchParams.utm_content}&utm_medium=${searchParams.utm_medium}`}
-
-                >{label}</a>
-            </div>
-        )
-    }
-
     if (searchParams.utm_campaign) {
         return (
             <div className={`w-full flex ${className ? className : 'max-w-sm'}`}>
                 <a
                     className={`btn-grad-2 px-4 py-3 text-xl font-bold uppercase rounded-full ${classNameB}`}
-                    href={`https://chk.eduzz.com/2337329/?utm_term=${searchParams.utm_term}&utm_campaign=${searchParams.utm_campaign}&utm_source=utm_source=${searchParams.utm_source}&utm_content=${searchParams.utm_content}&utm_medium=${searchParams.utm_medium}`}
+                    href={`https://chk.eduzz.com/2337329?utm_term=${searchParams.utm_term}&utm_campaign=${searchParams.utm_campaign}&utm_source=utm_source=${searchParams.utm_source}&utm_content=${searchParams.utm_content}&utm_medium=${searchParams.utm_medium}`}
 
                 >{label}</a>
             </div>
         )
     }
+
+    if (href) {
+        return (
+            <div className={`w-full flex ${className ? className : 'max-w-sm'}`}>
+                <a
+                    className={`px-16 sm:px-20 btn-grad-2 px-4 py-3 text-xl font-bold uppercase rounded-full ${classNameB}`}
+                    href={`${href}`}
+
+                >{label}</a>
+            </div>
+        )
+    }
+
 
     return (
         <div className={`w-full flex ${className ? className : 'max-w-sm'}`}>
